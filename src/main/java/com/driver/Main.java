8dbe263e089a2c967d8cece51b8837daa10dbf57
MushfiqueAlam <mushfique.037@gmail.com>
2025-01-22 01:23:32 +0530

    method overriding example

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,15 +3,9 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         B b=new B();
-
-//        Before override
-//        String str=b.meth();
-//        System.out.println(str);//get ouput of parent class A
-
-//        After override of method meth
-        System.out.println(b.meth());
-
-//        System.out.println(str);//get the output from child class after overriding.
+        b.meth();
+        B b1=new B();
+        b1.meth();
 
 
     }
